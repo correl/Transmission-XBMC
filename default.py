907e5c6300ffe773ea10eedf248025b5b778505f
Correl Roush <correl@gmail.com>
2010-06-22 13:05:53 -0400

    Version update

--- default.py
+++ default.py
@@ -9,7 +9,7 @@ __author__ = "Correl Roush <correl@gmail.com>"
 __url__ = "http://github.com/correl/Transmission-XBMC"
 __svn_url__ = ""
 __credits__ = ""
-__version__ = "0.5a"
+__version__ = "0.5.0"
 __XBMC_Revision__ = "22240"
 
 BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
