4d987017f9efe9d48b1f2942661254518e938e16
Correl Roush <correl@gmail.com>
2011-09-13 00:26:50 -0400

    Added search backends for btjunkie and thepiratebay

---
+++ resources/lib/search.py
@@ -0,0 +1,53 @@
+import re
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
+
+class Search:
+    def __init__(self):
+        return NotImplemented
+    def search(terms):
+        return NotImplemented
+
+class BTJunkie(Search):
+    def __init__(self):
+        self.search_uri = 'http://btjunkie.org/rss.xml?query={terms}&o=52'
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri.format(terms=terms)
+        f = urlopen(url)
+        soup = BeautifulStoneSoup(f.read())
+        for item in soup.findAll('item'):
+            (name, seeds, leechers) = re.findall('(.*?)\s+\[(\d+)\/(\d+)\]$', item.title.text)[0]
+            torrents.append({
+                'url': item.enclosure['url'],
+                'name': name,
+                'seeds': int(seeds),
+                'leechers': int(leechers),
+            })
+        return torrents
+class TPB(Search):
+    def __init__(self):
+        self.search_uri = 'http://thepiratebay.org/search/{terms}/'
+    def search(self, terms):
+        torrents = []
+        url = self.search_uri.format(terms=terms)
+        f = urlopen(url)
+        soup = BeautifulSoup(f.read())
+        for details in soup.findAll('a', {'class': 'detLink'}):
+            name = details.text
+            url = details.findNext('a', {'href': re.compile('^http:\/\/torrents\.thepiratebay\.org\/')})['href']
+            td = details.findNext('td')
+            seeds = int(td.text)
+            td = td.findNext('td')
+            leechers = int(td.text)
+            torrents.append({
+                'url': url,
+                'name': name,
+                'seeds': seeds,
+                'leechers': leechers,
+            })
+        return torrents
+
+if __name__ == '__main__':
+    s = TPB()
+    results = s.search('zettai')
