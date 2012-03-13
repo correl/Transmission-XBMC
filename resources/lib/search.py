a460bfab9770d1d8350921a1dbb11d56e06add19
Correl Roush <correl@gmail.com>
2012-03-13 19:34:47 -0400

    Removed btjunkie.org search

--- resources/lib/search.py
+++ resources/lib/search.py
@@ -11,27 +11,6 @@ class Search:
     def search(terms):
         return NotImplemented
 
-class BTJunkie(Search):
-    def __init__(self):
-        self.search_uri = 'http://btjunkie.org/rss.xml?query=%s&o=52'
-    def search(self, terms):
-        torrents = []
-        url = self.search_uri % '+'.join(terms.split(' '))
-        f = urlopen(url)
-        soup = BeautifulStoneSoup(f.read())
-        for item in soup.findAll('item'):
-            (name, seeds, leechers) = re.findall('(.*?)\s+\[(\d+|X)\/(\d+|X)\]$', item.title.text)[0]
-            if seeds == 'X':
-                seeds = 0
-            if leechers == 'X':
-                leechers = 0
-            torrents.append({
-                'url': item.enclosure['url'],
-                'name': name,
-                'seeds': int(seeds),
-                'leechers': int(leechers),
-            })
-        return torrents
 class Mininova(Search):
     def __init__(self):
         self.search_uri = 'http://www.mininova.org/rss/%s'
