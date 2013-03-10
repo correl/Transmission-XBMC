056a96e435e19f00e47c7b3a5af35b0810dcae64
Artem Glebov <artem.glebov@gmail.com>
2013-03-10 13:36:10 +0300

    Show the percentage next to the progress bar in the main window

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -89,7 +89,7 @@ class TransmissionGUI(xbmcgui.WindowXMLDialog):
             l.setLabel2(statusline)
             l.setProperty('TorrentStatusIcon', statusicons[torrent.status])
             l.setProperty('TorrentID', str(i))
-            l.setProperty('TorrentProgress', "%.2f" % torrent.progress)
+            l.setProperty('TorrentProgress', "%3d%%" % torrent.progress)
             l.setInfo('torrent', torrent.fields)
             l.setInfo('video', {'episode': int(torrent.progress)})
 
